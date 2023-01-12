a2e0ef1508623d8dafd16e3e853d24d69c245cea
Artem Sokolovskiy <artemsokol98@gmail.com>
2023-01-12 19:01:46 +0300

    Initial Commit

---
+++ ExchangeCurrencyTestTask/ExchangeCurrencyTestTaskApp.swift
@@ -0,0 +1,20 @@
+//
+//  ExchangeCurrencyTestTaskApp.swift
+//  ExchangeCurrencyTestTask
+//
+//  Created by Артем Соколовский on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeCurrencyTestTaskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
